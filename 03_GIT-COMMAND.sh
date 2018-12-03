fae49afa0bdc4aa6dcf456a9e9f11d7fcb466a0b
glory0510 <240949806@qq.com>
2018-12-03 13:28:54 +0800

    Update 03_GIT-COMMAND.sh

--- 03_GIT-COMMAND.sh
+++ 03_GIT-COMMAND.sh
@@ -45,6 +45,7 @@ git log            #查看提交历史
 git log --oneline  #以一行一次提交信息显示
 git log -p <file>  #查看指定文件的提交历史
 git blame <file>   #以列表的方式查看指定文件的提交历史
+git log --pretty=format:"%h %an %ar %s "
 
 #撤销
 git reset --hard HEAD        #撤销工作目录中所有未提交文件的修改内容
