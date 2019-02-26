62e62b6bd5cbdb39b6fed4d38ba3fa5f708cda17
glory0510 <240949806@qq.com>
2019-02-26 16:11:41 +0800

    test lfs

--- 04_LVM-RAID.sh
+++ 04_LVM-RAID.sh
@@ -10,3 +10,49 @@
 #可以将其视为数据的镜像或备份
 
 
+
+#LVM 
+fdisk -l  #查看分区类型，默认分区类型是以LVM类型分区的
+#如果当前磁盘没有完全被使用，则新建分区，
+fdisk /dev/sda
+Command (m for help): p    #查看分区表
+#创建好分区之后修改分区类型
+Command (m for help): t  
+Partition number (1,2, default 2): 2        #选择分区
+Hex code (type L to list all codes): L      #查看类型的code
+Hex code (type L to list all codes): 8e
+Command (m for help): w
+
+#如果分区已经被使用完，则挂载新的磁盘进行分区
+fdisk /dev/sdb
+
+#将新加的分区 /dev/sdb1 创建为物理卷
+pvcreate /dev/sdb1
+#查看物理卷
+pvdisplay
+
+
+#查看卷组列表，卷组名为centos
+vgdisplay
+
+
+#给卷组centos扩容
+vgextend centos  /dev/sdb1
+#再次查看卷组Free PE 已经增多
+vgdisplay
+
+#将卷组剩余的所有空间都扩展到lV(root)
+lvextend -l +100%FREE /dev/mapper/centos-root
+
+#查看文件系统
+cat /etc/fstab
+
+#写入文件系统使扩容生效
+xfs_growfs /dev/mapper/centos-root    #xfs文件系统
+resize2fs /dev/mapper/centos-root     #ext4文件系统
+#查看磁盘容量
+df -h 
+
+
+
+
